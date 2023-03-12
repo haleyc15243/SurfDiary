e2e0fe9a9f2b6f8b849977a556393e6b4e9e1ee6
Haley <richard.cunningham@maine.edu>
2023-03-12 10:02:13 -0400

    Remove build/ files accidentally committed before .gitignore was setup.

--- app/application/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_halebop_surfdiary_SurfDiaryApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.halebop.surfdiary.SurfDiaryApp"
-)
-public final class _com_halebop_surfdiary_SurfDiaryApp {
-}
