13d1b1d2461b145bea57fdffd02b8b910d50181a
haleyc15243 <richard.cunningham@maine.edu>
2024-01-13 10:21:41 -0500

    Convert the locations page to compose.

--- app/application/src/main/java/com/halebop/surfdiary/locations/LocationsFragment.kt
+++ app/application/src/main/java/com/halebop/surfdiary/locations/LocationsFragment.kt
@@ -1,11 +1,6 @@
 package com.halebop.surfdiary.locations
 
-import SurfDiaryTheme
-import android.os.Bundle
-import android.view.*
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,116 +10,40 @@ import androidx.compose.material.Checkbox
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircleOutline
+import androidx.compose.material.icons.filled.DeleteForever
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
-import com.halebop.surfdiary.application.R
 import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.core.view.MenuProvider
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.fragment.findNavController
 import com.halebop.surfdiary.LocationDatasource
+import com.halebop.surfdiary.application.R
 import com.halebop.surfdiary.ui.AppCardListItem
+import com.halebop.surfdiary.ui.AppTextEntry
 import com.halebop.surfdiary.ui.CircularBackgroundIcon
+import com.halebop.surfdiary.ui.SaveCancelDialog
+import com.halebop.surfdiary.ui.SurfDiaryAppState
+import com.halebop.surfdiary.ui.SurfDiaryScaffold
 import com.halebop.web_types.Location
-import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-
-@AndroidEntryPoint
-class LocationsFragment: Fragment() {
-
-    private val viewModel: LocationsViewModel by viewModels()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                SurfDiaryTheme {
-                    val stateHolder = rememberLocationListState(
-                        viewModel = viewModel,
-                        viewModelEventHandler = { event ->
-                            viewModel.onUiEvent(event)
-                        },
-                        fragmentEventHandler = { event ->
-                            when (event) {
-                                is LocationsFragmentFragmentUiEvent.LocationClicked -> {
-                                    //TODO
-                                }
-                            }
-                        }
-                    )
-                    LocationsFragmentScreen(stateHolder = stateHolder)
-                }
-            }
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        createMenu()
-    }
-
-    private fun createMenu() {
-        requireActivity().addMenuProvider(
-            object : MenuProvider {
-                override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-                    menuInflater.inflate(R.menu.menu_locations_fragment, menu)
-                    viewModel.editModeLiveData.observe(viewLifecycleOwner) { editMode ->
-                        val deleteButton = menu.findItem(R.id.menu_item_delete)
-                        deleteButton.isVisible = editMode
-                    }
-                }
-
-                override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-                    return when (menuItem.itemId) {
-                        R.id.menu_item_add -> {
-                            true.also { navigateToAddLocationFragment() }
-                        }
-                        R.id.menu_item_delete -> {
-                            true.also { viewModel.onUiEvent(LocationsFragmentViewModelUiEvent.DeleteLocations)}
-                        }
-                        else -> false
-                    }
-                }
-            },
-            viewLifecycleOwner,
-            Lifecycle.State.RESUMED
-        )
-    }
-
-    private fun navigateToAddLocationFragment() {
-        findNavController().navigate(LocationsFragmentDirections.actionLocationFragmentToAddLocationFragment())
-    }
-
-    @OptIn(ExperimentalLifecycleComposeApi::class)
-    @Composable
-    private fun rememberLocationListState(
-        viewModel: LocationsViewModel,
-        viewModelEventHandler: (LocationsFragmentViewModelUiEvent) -> Unit,
-        fragmentEventHandler: (LocationsFragmentFragmentUiEvent) -> Unit
-    ): LocationsFragmentStateHolder {
-        val locationListState = viewModel.uiStateFlow.collectAsStateWithLifecycle(initialValue = LocationsViewModel.LocationsListUiState.Initial, lifecycle)
-        return LocationsFragmentStateHolder(locationListState, viewModelEventHandler, fragmentEventHandler)
-    }
-}
 
 interface LocationsFragmentUiEvent
 
@@ -139,23 +58,92 @@ class LocationsFragmentStateHolder(
 )
 
 @Composable
-private fun LocationsFragmentScreen(
-    stateHolder: LocationsFragmentStateHolder
-) {
-    val state = stateHolder.state
-    LocationsListScreenContent(
-        state = state.value,
-        onUiEvent = { event ->
+private fun rememberLocationListState(
+    viewModel: LocationsViewModel,
+    lifecycle: Lifecycle,
+    viewModelEventHandler: (LocationsFragmentViewModelUiEvent) -> Unit,
+    fragmentEventHandler: (LocationsFragmentFragmentUiEvent) -> Unit
+): LocationsFragmentStateHolder {
+    val locationListState = viewModel.uiStateFlow.collectAsStateWithLifecycle(initialValue = LocationsViewModel.LocationsListUiState.Initial, lifecycle)
+    return LocationsFragmentStateHolder(locationListState, viewModelEventHandler, fragmentEventHandler)
+}
+
+@Composable
+fun LocationListScreen(appState: SurfDiaryAppState) {
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    val viewModel: LocationsViewModel = hiltViewModel()
+    val stateHolder = rememberLocationListState(
+        viewModel = viewModel,
+        lifecycle = lifecycle,
+        viewModelEventHandler = { event ->
+            viewModel.onUiEvent(event)
+        },
+        fragmentEventHandler = { event ->
             when (event) {
-                is LocationsFragmentFragmentUiEvent -> {
-                    stateHolder.fragmentEventHandler.invoke(event)
-                }
-                is LocationsFragmentViewModelUiEvent -> {
-                    stateHolder.viewModelEventHandler.invoke(event)
+                is LocationsFragmentFragmentUiEvent.LocationClicked -> {
+                    //TODO
                 }
             }
         }
     )
+    LocationsListScreen(
+        appState = appState,
+        state = stateHolder.state.value
+    ) { event ->
+        when (event) {
+            is LocationsFragmentFragmentUiEvent -> {
+                stateHolder.fragmentEventHandler.invoke(event)
+            }
+            is LocationsFragmentViewModelUiEvent -> {
+                stateHolder.viewModelEventHandler.invoke(event)
+            }
+        }
+    }
+}
+
+@Composable
+private fun LocationsListScreen(
+    appState: SurfDiaryAppState,
+    state: LocationsViewModel.LocationsListUiState,
+    onUiEvent: (LocationsFragmentUiEvent) -> Unit
+) {
+    SurfDiaryScaffold(
+        appState = appState,
+        topAppBarActions = {
+            when (state) {
+                LocationsViewModel.LocationsListUiState.Initial -> {
+                    // No toolbar icons
+                }
+                is LocationsViewModel.LocationsListUiState.UiState -> {
+                    val multiSelectMode = state.selectedLocationIds.isNotEmpty()
+                    if (multiSelectMode) {
+                        val showEditMenuItem = state.selectedLocationIds.size == 1
+                        EditModeToolbarIcons(
+                            showEdit = showEditMenuItem,
+                            edit = {
+                                onUiEvent.invoke(LocationsFragmentViewModelUiEvent.ShowLocationEditDialog)
+                            },
+                            delete = {
+                                onUiEvent.invoke(LocationsFragmentViewModelUiEvent.DeleteLocations)
+                            }
+                        )
+                    } else {
+                        IconButton(onClick = { appState.navigateToAddLocation() }) {
+                            Icon(
+                                imageVector = Icons.Filled.AddCircleOutline,
+                                contentDescription = "Add Location"
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    ) {
+        LocationsListScreenContent(
+            state = state,
+            onUiEvent = onUiEvent
+        )
+    }
 }
 
 @Composable
@@ -163,19 +151,55 @@ private fun LocationsListScreenContent(
     state: LocationsViewModel.LocationsListUiState,
     onUiEvent: (LocationsFragmentUiEvent) -> Unit
 ) {
-    Surface {
-        when (state) {
-            LocationsViewModel.LocationsListUiState.Initial -> CircularProgressIndicator()
-            is LocationsViewModel.LocationsListUiState.UiState -> {
-                LocationItemList(
-                    locationList = state.locations,
-                    selectedLocationIds = state.selectedLocationIds,
-                    addSelectedLocation = { onUiEvent.invoke(LocationsFragmentViewModelUiEvent.SetSelected(true, it.id)) },
-                    removeSelectedLocation = { onUiEvent.invoke(LocationsFragmentViewModelUiEvent.SetSelected(false, it.id)) },
-                    onUiEvent = onUiEvent
+    when (state) {
+        LocationsViewModel.LocationsListUiState.Initial -> CircularProgressIndicator()
+        is LocationsViewModel.LocationsListUiState.UiState -> {
+            if (state.showEditLocationDialog) {
+                EditLocationDialog(
+                    currentName = "", // TODO state.,
+                    updateLocation = {
+                        onUiEvent.invoke(LocationsFragmentViewModelUiEvent.UpdateLocationName(it))
+                    },
+                    onDismissRequest = {
+                        onUiEvent.invoke(LocationsFragmentViewModelUiEvent.ShowLocationEditDialog)
+                    }
                 )
             }
+            LocationItemList(
+                locationList = state.locations,
+                selectedLocationIds = state.selectedLocationIds,
+                addSelectedLocation = { onUiEvent.invoke(LocationsFragmentViewModelUiEvent.SetSelected(true, it.id)) },
+                removeSelectedLocation = { onUiEvent.invoke(LocationsFragmentViewModelUiEvent.SetSelected(false, it.id)) },
+                onUiEvent = onUiEvent
+            )
         }
+    }
+}
+
+@Composable
+private fun EditLocationDialog(
+    currentName: String,
+    updateLocation: (String) -> Unit,
+    onDismissRequest: () -> Unit
+) {
+    val currentText = rememberSaveable { mutableStateOf("") }
+    SaveCancelDialog(
+        onSavePressed = { updateLocation.invoke(currentText.value) },
+        onDismissRequest = onDismissRequest
+    ) {
+        AppTextEntry(
+            currentText = currentText.value,
+            onValueChange = {
+                currentText.value = it
+            },
+            enabled = true,
+            label = {
+                Text(stringResource(R.string.label_location_name))
+            },
+            placeholder = {
+                Text(currentName)
+            }
+        )
     }
 }
 
@@ -249,8 +273,32 @@ private fun LocationItem(
     }
 }
 
+@Composable
+private fun EditModeToolbarIcons(
+    showEdit: Boolean,
+    edit: () -> Unit,
+    delete: () -> Unit
+) {
+    if (showEdit) {
+        IconButton(onClick = edit) {
+            Icon(
+                imageVector = Icons.Filled.Edit,
+                contentDescription = "Add Location"
+            )
+        }
+    }
+    IconButton(onClick = delete) {
+        Icon(
+            imageVector = Icons.Filled.DeleteForever,
+            contentDescription = "Add Location"
+        )
+    }
+}
+
 sealed class LocationsFragmentViewModelUiEvent: LocationsFragmentUiEvent {
-    object DeleteLocations: LocationsFragmentViewModelUiEvent()
+    data object ShowLocationEditDialog: LocationsFragmentViewModelUiEvent()
+    data class UpdateLocationName(val newName: String): LocationsFragmentViewModelUiEvent()
+    data object DeleteLocations: LocationsFragmentViewModelUiEvent()
     data class SetSelected(val selected: Boolean, val id: Long): LocationsFragmentViewModelUiEvent()
 }
 
@@ -259,9 +307,18 @@ class LocationsViewModel @Inject constructor(
     private val locationDatasource: LocationDatasource,
     private val savedStateHandle: SavedStateHandle
 ): ViewModel() {
+    private val showEditLocationDialogFlow = MutableStateFlow(false)
+    private val selectedLocationIds = MutableStateFlow<List<Long>>(savedStateHandle["selectedLocationIds"] ?: emptyList())
+    private val locationsFlow = locationDatasource.locationsFlow()
 
     fun onUiEvent(event: LocationsFragmentViewModelUiEvent) {
         when (event) {
+            LocationsFragmentViewModelUiEvent.ShowLocationEditDialog -> {
+                showEditLocationDialogFlow.value = !showEditLocationDialogFlow.value
+            }
+            is LocationsFragmentViewModelUiEvent.UpdateLocationName -> {
+                // TODO locationDatasource.
+            }
             is LocationsFragmentViewModelUiEvent.DeleteLocations -> {
                 for (id in selectedLocationIds.value) {
                     selectedLocationIds.value = selectedLocationIds.value.minus(id)
@@ -276,19 +333,20 @@ class LocationsViewModel @Inject constructor(
         }
     }
 
-    fun saveState() {
-        savedStateHandle["selectedLocationIds"] = selectedLocationIds.value
-    }
-
     sealed class LocationsListUiState {
-        object Initial: LocationsListUiState()
-        data class UiState(val locations: List<Location>, val selectedLocationIds: List<Long>): LocationsListUiState()
+        data object Initial: LocationsListUiState()
+        data class UiState(
+            val locations: List<Location>,
+            val selectedLocationIds: List<Long>,
+            val showEditLocationDialog: Boolean
+        ): LocationsListUiState()
     }
-    private val selectedLocationIds = MutableStateFlow<List<Long>>(savedStateHandle["selectedLocationIds"] ?: emptyList())
-    private val locationsFlow = locationDatasource.locationsFlow()
 
-    val editModeLiveData = selectedLocationIds.map { it.isNotEmpty() }.asLiveData()
-    val uiStateFlow = combine(locationsFlow, selectedLocationIds) { locationsList, selectedIds ->
-        LocationsListUiState.UiState(locationsList, selectedIds)
+    val uiStateFlow = combine(
+        locationsFlow,
+        selectedLocationIds,
+        showEditLocationDialogFlow
+    ) { locationsList, selectedIds, showEditLocationDialog ->
+        LocationsListUiState.UiState(locationsList, selectedIds, showEditLocationDialog)
     }
 }
