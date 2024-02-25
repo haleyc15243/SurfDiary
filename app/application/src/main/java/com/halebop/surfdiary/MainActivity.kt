97a37d7c88e295a8d267a3c2e4ce1c22a9eb0be7
haleyc15243 <richard.cunningham@maine.edu>
2024-02-25 07:44:49 -0500

    Schedule the active stations worker to refresh the list each time the app starts. This will likely be tweaked in the future to avoid running too often.

--- app/application/src/main/java/com/halebop/surfdiary/MainActivity.kt
+++ app/application/src/main/java/com/halebop/surfdiary/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import com.halebop.surfdiary.ui.SurfDiaryAppState
 import com.halebop.surfdiary.ui.SurfDiaryNavigation
 import com.halebop.surfdiary.ui.rememberSurfDiaryAppState
 import com.halebop.surfdiary.ui.theme.SurfDiaryTheme
+import com.halebop.surfdiary.work.NDBCActiveStationsWorker
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,7 +24,29 @@ class MainActivity : ComponentActivity() {
                 SurfDiaryApp()
             }
         }
+
+        schedulePeriodicWork()
     }
+
+    private fun schedulePeriodicWork() {
+        WorkManager.getInstance(this).apply {
+            for (request in onStartUpWorkers()) {
+                enqueue(request)
+            }
+        }
+    }
+
+    private fun onStartUpWorkers() =
+        listOf(
+            ndbcActiveStationsWorkRequest()
+        )
+
+    private fun ndbcActiveStationsWorkRequest() =
+        OneTimeWorkRequestBuilder<NDBCActiveStationsWorker>()
+            .setConstraints(
+                Constraints(requiredNetworkType = NetworkType.CONNECTED)
+            )
+            .build()
 }
 
 @Composable
