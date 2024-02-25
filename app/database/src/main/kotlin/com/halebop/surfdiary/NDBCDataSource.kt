3522d96f0b0d3a5908ec011ae1e7b937e01e63b0
haleyc15243 <richard.cunningham@maine.edu>
2024-02-25 07:35:52 -0500

    Create database tables with matching datasource to store NDBC active station responses.

---
+++ app/database/src/main/kotlin/com/halebop/surfdiary/NDBCDataSource.kt
@@ -0,0 +1,42 @@
+package com.halebop.surfdiary
+
+import com.halebop.surfdiary.database.Ndbc_stationQueries
+import com.halebop.web_types.NDBC
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
+
+interface NDBCDataSource {
+    fun updateLastUpdate(lastUpdate: Instant)
+    fun getActiveStations(): Flow<List<NDBC.Station>>
+    fun insertStation(station: NDBC.Station)
+}
+
+class NDBCDataSourceImpl(
+    private val stationQueries: Ndbc_stationQueries
+) : NDBCDataSource {
+
+    override fun updateLastUpdate(lastUpdate: Instant) {
+        stationQueries.updateListUpdated(lastUpdate)
+    }
+    override fun getActiveStations(): Flow<List<NDBC.Station>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun insertStation(station: NDBC.Station) {
+        stationQueries.insertOrUpdateStation(
+            station.id,
+            station.latLng.lat,
+            station.latLng.lng,
+            station.elevation,
+            station.name,
+            station.owner,
+            station.program,
+            station.type,
+            station.seq,
+            station.hasMeteorologicalData,
+            station.hasCurrents,
+            station.hasWaterQuality,
+            station.hasDart
+        )
+    }
+}
